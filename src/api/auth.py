63710224e14a907f9465411f6cd9e5692a75cafb
anton <anton@example.com>
2025-04-05 23:09:17 +0500

    only_auth

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,14 +1,28 @@
-from fastapi import APIRouter
-from passlib.context import CryptContext
+from fastapi import APIRouter, HTTPException, Response, Request
+
 
 from repositories.users import UsersRepository
 from schemas.users import UserAdd, UserRequestAdd
 from src.database import async_session_maker
+from services.auth import AuthService
 
 router = APIRouter(prefix="/auth", tags=["Аавторизация и аутентификация"])
 
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated = "auto")
+@router.post("/login")
+async def login_user(
+    data: UserRequestAdd,
+    response: Response
+):
+    async with async_session_maker() as session:
+        user = await UsersRepository(session).get_user_with_hashed_password(email = data.email)
+        if not user:
+            raise HTTPException(status_code=401, detail="Пользователь с таким email не зарегистррован")
+        if not AuthService().verify_password(data.password, user.hashed_password):
+            raise HTTPException(status_code=401, detail="Пароль неверный")
+        access_token = AuthService().create_access_token({"user_id": user.id})
+        response.set_cookie("access_token", access_token)
+        return {"access_token": access_token}
 
 
 
@@ -16,9 +30,17 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated = "auto")
 async def register_user(
     data: UserRequestAdd,
 ):
-    hashed_password = pwd_context.hash(data.password)
+    hashed_password = AuthService().hash_password(data.password)
     new_user_data = UserAdd(email = data.email, hashed_password=hashed_password)
     async with async_session_maker() as session:
         await UsersRepository(session).add(new_user_data)
         await session.commit()
     return {'status': 'OK'}
+
+
+@router.get("/only_auth")
+async def only_auth(
+    request: Request
+):
+    access_token = request.cookies.get("access_token", None)
+
--- src/repositories/users.py
+++ src/repositories/users.py
@@ -1,7 +1,15 @@
+from sqlalchemy import select
+
 from repositories.base import BaseRepository
-from schemas.users import User
+from schemas.users import User, UserWithHashedPassword
 from src.models.users import UsersOrm
 
 class UsersRepository(BaseRepository):
     model = UsersOrm
     schema = User
+
+    async def get_user_with_hashed_password(self, email):
+        query = select(self.model).filter_by(email = email)
+        result = await self.session.execute(query)
+        model = result.scalars().one()
+        return UserWithHashedPassword.model_validate(model, from_attributes=True)
