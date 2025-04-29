efa5a01e11baa0153b4f5292216ffc32b3284a83
anton <anton@example.com>
2025-04-29 19:23:58 +0500

    DBDep

--- src/api/dependencies.py
+++ src/api/dependencies.py
@@ -2,8 +2,10 @@ from typing import Annotated
 
 from fastapi import Depends, Query, HTTPException, Request
 from pydantic import BaseModel
+from src.database import async_session_maker
 
 from services.auth import AuthService
+from utils.db_manager import DBManager
 
 
 class PaginationParams(BaseModel):
@@ -21,6 +23,16 @@ def get_token(request: Request) -> str:
 def get_current_user_id(token: str = Depends(get_token)) -> int:
     data = AuthService().decode_token(token)
     return data["user_id"]
-    
 
+    
 UserIdDep = Annotated[int, Depends(get_current_user_id)]
+
+def get_db_manager():
+    return DBManager(session_factory=async_session_maker)
+
+
+async def get_db():
+    async with get_db_manager() as db:
+        yield db
+
+DBDep = Annotated[DBManager, Depends(get_db)]       
