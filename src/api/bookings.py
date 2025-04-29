a34d6e09b21e0b9da1a50e09cf589965b13488b9
anton <anton@example.com>
2025-04-29 23:22:37 +0500

    get_bookings

--- src/api/bookings.py
+++ src/api/bookings.py
@@ -1,4 +1,3 @@
-from datetime import date
 from fastapi import APIRouter
 
 from src.schemas.bookings import BookingAddRequest, BookingAdd
@@ -19,3 +18,15 @@ async def create_booking(booking_data: BookingAddRequest, user_id: UserIdDep, db
     booking = await db.bookings.add(_booking_data)
     await db.commit()
     return {"status": "OK", "data": booking}
+
+
+@router.get("")
+async def get_bookings(db: DBDep):
+    return await db.bookings.get_all()
+
+@router.get("/me")
+async def get_bookings_by_user(
+    db: DBDep,
+    user_id: UserIdDep
+):
+    return await db.bookings.get_filtered(user_id = user_id)
