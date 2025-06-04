62aff77035b7902d0ccdddb1a4b6698d4f5033d2
anton <anton@example.com>
2025-06-04 16:22:52 +0500

    crud_booking

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,11 +15,19 @@ import json
 def check_test_mode():
     assert settings.MODE == "TEST"
 
+
+@pytest.fixture(scope='function')
+async def db():
+    async with DBManager(session_factory=async_session_maker_null_pool) as db:
+        yield db
+
+
 @pytest.fixture(scope="session", autouse=True)
-async def setup_database():
+async def setup_database(check_test_mode):
     async with engine_null_pool_test.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
+
     with open("tests/mock_hotels.json", encoding="utf-8") as file_hotels:
         hotels = json.load(file_hotels)
     with open("tests/mock_rooms.json", encoding="utf-8") as file_rooms:
@@ -27,20 +35,25 @@ async def setup_database():
 
     hotels = [HotelAdd.model_validate(hotel) for hotel in hotels]
     rooms = [RoomAdd.model_validate(room) for room in rooms]
+    async with DBManager(session_factory=async_session_maker_null_pool) as db_:
+        await db_.hotels.add_bulk(hotels)
+        await db_.rooms.add_bulk(rooms)
+        await db_.commit()
 
-    async with DBManager(session_factory=async_session_maker_null_pool) as db:
-        await db.hotels.add_bulk(hotels)
-        await db.rooms.add_bulk(rooms)
-        await db.commit()
+@pytest.fixture(scope="session")
+async def ac():
+    async with AsyncClient(transport=ASGITransport(app=app), base_url= 'http://test') as ac:
+        yield ac
 
 
 @pytest.fixture(scope="session", autouse=True)
-async def register_user():
-    async with AsyncClient(transport=ASGITransport(app=app), base_url= 'http://test') as ac:
-        await ac.post(
-            "/auth/register",
-            json={
-                "email": "kot@pes.com",
-                "password": "1234"
-            }
-        )
+async def register_user(ac,setup_database):
+    await ac.post(
+        "/auth/register",
+        json={
+            "email": "kot@pes.com",
+            "password": "1234"
+        }
+    )
+
+
