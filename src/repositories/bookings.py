a89b07c044973634150cb747e8b8846555a60454
anton <anton@example.com>
2025-06-18 18:33:01 +0500

    'check_booking'

--- src/repositories/bookings.py
+++ src/repositories/bookings.py
@@ -4,6 +4,8 @@ from src.models.bookings import BookingsOrm
 from src.schemas.bookings import Booking
 from sqlalchemy import select
 from datetime import date
+from src.repositories.utils import rooms_ids_for_booking
+from src.schemas.bookings import BookingAdd
 
 class BookingsRepository(BaseRepository):
     model = BookingsOrm
@@ -16,3 +18,18 @@ class BookingsRepository(BaseRepository):
         )
         res = await self.session.execute(query)
         return [self.mapper.map_to_domain_entity(booking) for booking in res.scalars().all()]
+    
+    async def add_booking(self, data: BookingAdd, hotel_id: int):
+        rooms_ids_to_get = rooms_ids_for_booking(
+            date_from=data.date_from,
+            date_to=data.date_to,
+            hotel_id=hotel_id,
+        )
+        rooms_ids_to_book_res = await self.session.execute(rooms_ids_to_get)
+        rooms_ids_to_book: list[int] = rooms_ids_to_book_res.scalars().all()
+
+        if data.room_id in rooms_ids_to_book:
+            new_booking = await self.add(data)
+            return new_booking
+        else:
+            raise Exception
